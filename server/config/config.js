1627cbaa01307288ccf8a7c837320058e20b5dcc
roxana <tiscarenoroxana@gmail.com>
2020-06-01 12:18:56 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //======================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//======================
+//Entorno
+//======================
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//======================
+//Base de datos
+//======================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://161895:E4k6zKbnFKqAdApG@cluster0-jxhgp.mongodb.net/cafe';
+}
+process.env.urlDB = urlDB;
